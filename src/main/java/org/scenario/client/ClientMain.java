74aee4789cf9163d3bd96c7ec1df42f61c60f69a
Janaka Pushpakumara <bajpushpakumara@gmail.com>
2023-05-09 13:56:05 +0530

    Initial commit

---
+++ src/main/java/org/scenario/client/ClientMain.java
@@ -0,0 +1,7 @@
+package org.scenario.client;
+
+public class ClientMain {
+    public static void main(String[] args) {
+        System.out.println("Starting the Test Client main!");
+    }
+}
---
+++ src/main/java/org/scenario/server/ServerMain.java
@@ -0,0 +1,7 @@
+package org.scenario.server;
+
+public class ServerMain {
+    public static void main(String[] args) {
+        System.out.println("Starting the Test Server main!");
+    }
+}
