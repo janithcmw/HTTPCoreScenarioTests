acd3ca3f39a3193322bce3b0941141875965fbcd
Janaka Pushpakumara <bajpushpakumara@gmail.com>
2023-05-10 13:07:27 +0530

    Adding blocking and nonblocking simple clients

--- src/main/java/org/scenarios/client/ClientMain.java
+++ src/main/java/org/scenarios/client/ClientMain.java
@@ -1,7 +1,9 @@
 package org.scenarios.client;
 
 import org.scenarios.client.helpers.RequestMethods;
-import org.scenarios.client.helpers.TestPayloads;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class ClientMain {
 
@@ -11,20 +13,46 @@ public class ClientMain {
     public static String Bearer = "eyJ4NXQiOiJNell4TW1Ga09HWXdNV0kwWldObU5EY3hOR1l3WW1NNFpUQTNNV0kyTkRBelpHUXpOR00wWkdSbE5qSmtPREZrWkRSaU9URmtNV0ZoTXpVMlpHVmxOZyIsImtpZCI6Ik16WXhNbUZrT0dZd01XSTBaV05tTkRjeE5HWXdZbU00WlRBM01XSTJOREF6WkdRek5HTTBaR1JsTmpKa09ERmtaRFJpT1RGa01XRmhNelUyWkdWbE5nX1JTMjU2IiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiJhZG1pbkBjYXJib24uc3VwZXIiLCJhdXQiOiJBUFBMSUNBVElPTiIsImF1ZCI6Ik1iX09sU1NYSDV6cWZmR1BRVDhLc29SX1VfWWEiLCJuYmYiOjE2ODM2OTY0MjcsImF6cCI6Ik1iX09sU1NYSDV6cWZmR1BRVDhLc29SX1VfWWEiLCJzY29wZSI6ImRlZmF1bHQiLCJpc3MiOiJodHRwczpcL1wvbG9jYWxob3N0Ojk0NDNcL29hdXRoMlwvdG9rZW4iLCJleHAiOjkyMjMzNzIwMzY4NTQ3NzUsImlhdCI6MTY4MzY5NjQyNywianRpIjoiZDQ4Y2UxMWQtZGZkZS00Y2U2LThlODAtYTAwMzRiMmJhMzY5In0.s5R2TztUdSQWufvNlPjfT8jB_BPHl4XS4bbyUE5UyL-4GkW2BWkJNnyM7MhBGfqDXCIZ5k7qDfuGEYVzNq9RBLtOkjxqAR-npHHyhXvUD18gLcosHP76VMKj1tkbQqyPGtuq4ENJezxda9KYRTne8n995M7ypZiRByG-1OL4f3jzqacsDghzdK-sQ4BUF6KBj3GAhoaG0RfDntOgS4AMDOpAF1umuoVxPMz81KrHNJtRBvt8ld7x68cFhbY1cEpkf2RIH_nsQfH8A4mAC27GfIBTs3L40h5YEHGONz5w9mbg_ysChA0mduuYQVOkHw_-53VEdp5DRTT3C1P5Ee-I0g";
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Starting the Test Client main!");
-
         ClientMain main = new ClientMain();
-
-        main.runHTTPClientsWithPayload("localhost", serverPort, TestPayloads.SMALL_PAYLOAD, RequestMethods.POST);
+        String content = readFile(" < Path to 1MB.json >");
+        main.runHTTPClientsWithPayload("localhost", serverPort,content , RequestMethods.POST);
     }
-
     public void runHTTPClientsWithPayload(String host, int port, String payload, RequestMethods method) {
 
-        System.out.println("Run ClientSendContentLessThanContentLength");
-        ClientSendContentLessThanContentLength client1 = new ClientSendContentLessThanContentLength(host, port, Bearer);
-        client1.run(payload, method);
-        System.out.println("Stop ClientSendContentLessThanContentLength");
+//        System.out.println("Run ClientSendContentLessThanContentLength");
+//        ClientSendContentLessThanContentLength clientSendContentLessThanContentLength = new ClientSendContentLessThanContentLength(host, port, Bearer);
+//        clientSendContentLessThanContentLength.run(payload, method);
+//        System.out.println("Stop ClientSendContentLessThanContentLength");
+//
+//        System.out.println("Run SimpleBlockingClient");
+//        SimpleBlockingClient simpleBlockingClient = new SimpleBlockingClient(host, port, Bearer);
+//        simpleBlockingClient.run(payload, method);
+//        System.out.println("Stop SimpleBlockingClient");
+
+        System.out.println("Run SimpleNonBlockingClient");
+        SimpleNonBlockingClient simpleNonBlockingClient = new SimpleNonBlockingClient(host, port, Bearer);
+        simpleNonBlockingClient.run(payload, method);
+        System.out.println("Stop SimpleNonBlockingClient");
 
     }
+    public static String readFile(String fileLocation) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(fileLocation));
+        try {
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+
+            while (line != null) {
+                sb.append(line);
+                sb.append(System.lineSeparator());
+                line = br.readLine();
+            }
+            String everything = sb.toString();
+            return everything;
+        } finally {
+            br.close();
+        }
+    }
+
 }
---
+++ src/main/java/org/scenarios/client/SimpleNonBlockingClient.java
@@ -0,0 +1,134 @@
+package org.scenarios.client;
+
+import org.scenarios.client.helpers.RequestMethods;
+
+import javax.net.ssl.*;
+import java.io.*;
+import java.security.KeyStore;
+
+public class SimpleNonBlockingClient {
+    private static String Bearer;
+    private final String host;
+    private final int port;
+
+    public SimpleNonBlockingClient(String host, int port, String Bearer) {
+        this.Bearer = Bearer;
+        this.host = host;
+        this.port = port;
+    }
+
+    public void run(String payload, RequestMethods method) {
+        try {
+            // Create ssl socket
+            SSLContext sslContext = this.createSSLContext();
+            try {
+                // Create socket factory
+                SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+                // Create socket
+                SSLSocket sslSocket = (SSLSocket) sslSocketFactory.createSocket(this.host, this.port);
+                System.out.println("Client " + this.getClass().getName() + " started");
+                new SimpleNonBlockingClient.ClientThread(sslSocket, payload, method).start();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        } catch (Exception ex) {
+        ex.printStackTrace();
+    }
+    }
+    static class ClientThread extends Thread {
+
+            private final SSLSocket sslSocket;
+            private final String payload;
+            RequestMethods method;
+
+            ClientThread(SSLSocket sslSocket, String payload, RequestMethods method) {
+                this.sslSocket = sslSocket;
+                this.payload = payload;
+                this.method = method;
+            }
+
+            public void run() {
+                sslSocket.setEnabledCipherSuites(sslSocket.getSupportedCipherSuites());
+                try {
+                    // Start handshake
+                    sslSocket.startHandshake();
+                    // Get session after the connection is established
+                    SSLSession sslSession = sslSocket.getSession();
+                    System.out.println("SSLSession :");
+                    System.out.println("\tProtocol : " + sslSession.getProtocol());
+                    System.out.println("\tCipher suite : " + sslSession.getCipherSuite());
+                    // Start handling application content
+                    OutputStream outputStream = sslSocket.getOutputStream();
+                    // Create a thread to read the response
+                    Thread responseThread = new Thread(new ResponseReader(sslSocket));
+                    responseThread.start();
+
+                    PrintStream printWriter = new PrintStream(outputStream);
+
+                    // Write header data
+                    printWriter.print(method + " /test/1 HTTP/1.1\r\n ");
+                    printWriter.print("Accept: application/json\r\n");
+                    printWriter.print("Connection: keep-alive\r\n");
+                    printWriter.print("Authorization: Bearer "+ Bearer +"\r\n");
+                    printWriter.print("Content-Type: application/json\r\n");
+                    printWriter.print("content-length: 1048576\r\n");
+                    printWriter.print("\r\n");
+                    // Write payload data
+                    printWriter.print(payload);
+                    printWriter.print("\r\n");
+                    printWriter.flush();
+                    sslSocket.close();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+
+        private static class ResponseReader implements Runnable {
+            private final SSLSocket sslSocket;
+            public ResponseReader(SSLSocket sslSocket) {
+                this.sslSocket = sslSocket;
+            }
+            @Override
+            public void run() {
+                try {
+                    InputStream inputStream = sslSocket.getInputStream();
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                    String line;
+                    while((line = bufferedReader.readLine()) != null){
+                        System.out.println("Response : "+line);
+                    }
+
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+    private SSLContext createSSLContext() {
+
+        try {
+            KeyStore keyStore = KeyStore.getInstance("JKS");
+            keyStore.load(new FileInputStream(ClientMain.keyStoreLocation), ClientMain.keyStorePassword.toCharArray());
+
+            // Create key manager
+            KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("SunX509");
+            keyManagerFactory.init(keyStore, "wso2carbon".toCharArray());
+            KeyManager[] km = keyManagerFactory.getKeyManagers();
+
+            // Create trust manager
+            TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance("SunX509");
+            trustManagerFactory.init(keyStore);
+            TrustManager[] tm = trustManagerFactory.getTrustManagers();
+
+            // Initialize SSLContext
+            SSLContext sslContext = SSLContext.getInstance("TLSv1.2");
+            sslContext.init(km, tm, null);
+
+            return sslContext;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return null;
+    }
+}
