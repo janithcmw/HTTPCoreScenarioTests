bb7da55eb12b349acc997dba132e3e148c560f6d
Janaka Pushpakumara <37293166+JanakaPushpakumara@users.noreply.github.com>
2023-05-10 11:18:05 +0530

    Delete src/main/java/org/scenario directory

--- src/main/java/org/scenario/client/ClientMain.java
+++
@@ -1,7 +0,0 @@
-package org.scenario.client;
-
-public class ClientMain {
-    public static void main(String[] args) {
-        System.out.println("Starting the Test Client main!");
-    }
-}
--- src/main/java/org/scenario/server/ServerMain.java
+++
@@ -1,7 +0,0 @@
-package org.scenario.server;
-
-public class ServerMain {
-    public static void main(String[] args) {
-        System.out.println("Starting the Test Server main!");
-    }
-}
