7d357bb84342624a2f4f4a4f2dc886c87aa9b4ae
Janaka Pushpakumara <bajpushpakumara@gmail.com>
2023-05-10 14:51:40 +0530

    adding BlockingClientSendContentLessThanContentLength

---
+++ src/main/java/org/scenarios/client/BlockingClientSendContentLessThanContentLength.java
@@ -0,0 +1,123 @@
+package org.scenarios.client;
+
+import org.scenarios.client.helpers.RequestMethods;
+
+import javax.net.ssl.*;
+import java.io.*;
+import java.security.KeyStore;
+
+public class BlockingClientSendContentLessThanContentLength {
+    private static String Bearer;
+    private final String host;
+    private final int port;
+    public static final String CRLF = "\r\n";
+
+    public BlockingClientSendContentLessThanContentLength(String host, int port, String Bearer) {
+        this.Bearer = Bearer;
+        this.host = host;
+        this.port = port;
+    }
+
+    public void run(String payload, RequestMethods method) {
+        try {
+            // Create ssl socket
+            SSLContext sslContext = this.createSSLContext();
+            try {
+                // Create socket factory
+                SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+                // Create socket
+                SSLSocket sslSocket = (SSLSocket) sslSocketFactory.createSocket(this.host, this.port);
+                System.out.println("Client " + this.getClass().getName() + " started");
+                new BlockingClientSendContentLessThanContentLength.ClientThread(sslSocket, payload, method).start();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        } catch (Exception ex) {
+        ex.printStackTrace();
+    }
+    }
+    static class ClientThread extends Thread {
+
+        private SSLSocket sslSocket;
+        private String payload;
+        RequestMethods method;
+
+        ClientThread(SSLSocket sslSocket, String payload, RequestMethods method) {
+            this.sslSocket = sslSocket;
+            this.payload = payload;
+            this.method = method;
+        }
+
+        public void run() {
+            sslSocket.setEnabledCipherSuites(sslSocket.getSupportedCipherSuites());
+            try {
+                // Start handshake
+                sslSocket.startHandshake();
+                // Get session after the connection is established
+                SSLSession sslSession = sslSocket.getSession();
+                System.out.println("SSLSession :");
+                System.out.println("\tProtocol : " + sslSession.getProtocol());
+                System.out.println("\tCipher suite : " + sslSession.getCipherSuite());
+                // Start handling application content
+                OutputStream outputStream = sslSocket.getOutputStream();
+                InputStream inputStream = sslSocket.getInputStream();
+                // Create a thread to read the response
+
+                PrintStream printWriter = new PrintStream(outputStream);
+
+                // Write data
+                printWriter.print(method + " /test/1 HTTP/1.1\r\n ");
+                printWriter.print("Accept: application/json\r\n");
+                printWriter.print("Connection: keep-alive\r\n");
+                printWriter.print("Authorization: Bearer " + Bearer + "\r\n");
+                printWriter.print("Content-Type: application/json\r\n");
+                printWriter.print("content-length: 1048576\r\n");
+                printWriter.print("\r\n");
+                printWriter.print(payload);
+                // Remove the eol
+                //printWriter.print("\r\n");
+                printWriter.flush();
+                Thread.sleep(650000);
+                // Never send the last message
+                printWriter.print("\r\n");
+
+                // Never Read the response
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    System.out.println("Response : " + line);
+                }
+                sslSocket.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+    private SSLContext createSSLContext() {
+
+        try {
+            KeyStore keyStore = KeyStore.getInstance("JKS");
+            keyStore.load(new FileInputStream(ClientMain.keyStoreLocation), ClientMain.keyStorePassword.toCharArray());
+
+            // Create key manager
+            KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("SunX509");
+            keyManagerFactory.init(keyStore, "wso2carbon".toCharArray());
+            KeyManager[] km = keyManagerFactory.getKeyManagers();
+
+            // Create trust manager
+            TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance("SunX509");
+            trustManagerFactory.init(keyStore);
+            TrustManager[] tm = trustManagerFactory.getTrustManagers();
+
+            // Initialize SSLContext
+            SSLContext sslContext = SSLContext.getInstance("TLSv1.2");
+            sslContext.init(km, tm, null);
+
+            return sslContext;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return null;
+    }
+}
