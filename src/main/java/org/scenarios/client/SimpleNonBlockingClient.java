2c42747316bd3b372da34b5d5727f4e9dac6ced8
Janaka Pushpakumara <bajpushpakumara@gmail.com>
2023-05-13 14:08:30 +0530

    Close the socket after reading the response

--- src/main/java/org/scenarios/client/SimpleNonBlockingClient.java
+++ src/main/java/org/scenarios/client/SimpleNonBlockingClient.java
@@ -78,7 +78,8 @@ public class SimpleNonBlockingClient {
                     printWriter.print(payload);
                     printWriter.print("\r\n");
                     printWriter.flush();
-                    sslSocket.close();
+                    printWriter.close();
+                    // closing the socket after reading the response
                 } catch (Exception ex) {
                     ex.printStackTrace();
                 }
@@ -99,7 +100,7 @@ public class SimpleNonBlockingClient {
                     while((line = bufferedReader.readLine()) != null){
                         System.out.println("Response : "+line);
                     }
-
+                    sslSocket.close();
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
