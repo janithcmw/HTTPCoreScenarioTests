aa9892c0a0c928bc421d9ce6fa83a61cb2af19a3
Janaka Pushpakumara <bajpushpakumara@gmail.com>
2023-05-15 00:11:43 +0530

    Update SimpleNonBlockingClient

--- src/main/java/org/scenarios/client/SimpleNonBlockingClient.java
+++ src/main/java/org/scenarios/client/SimpleNonBlockingClient.java
@@ -26,37 +26,16 @@ public class SimpleNonBlockingClient {
                 SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
                 // Create socket
                 SSLSocket sslSocket = (SSLSocket) sslSocketFactory.createSocket(this.host, this.port);
-                System.out.println("Client " + this.getClass().getName() + " started");
-                new SimpleNonBlockingClient.ClientThread(sslSocket, payload, method).start();
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        } catch (Exception ex) {
-        ex.printStackTrace();
-    }
-    }
-    static class ClientThread extends Thread {
-
-            private final SSLSocket sslSocket;
-            private final String payload;
-            RequestMethods method;
-
-            ClientThread(SSLSocket sslSocket, String payload, RequestMethods method) {
-                this.sslSocket = sslSocket;
-                this.payload = payload;
-                this.method = method;
-            }
-
-            public void run() {
+                //System.out.println("Client " + this.getClass().getName() + " started");
                 sslSocket.setEnabledCipherSuites(sslSocket.getSupportedCipherSuites());
                 try {
                     // Start handshake
                     sslSocket.startHandshake();
                     // Get session after the connection is established
                     SSLSession sslSession = sslSocket.getSession();
-                    System.out.println("SSLSession :");
-                    System.out.println("\tProtocol : " + sslSession.getProtocol());
-                    System.out.println("\tCipher suite : " + sslSession.getCipherSuite());
+                    //System.out.println("SSLSession :");
+                    //System.out.println("\tProtocol : " + sslSession.getProtocol());
+                    //System.out.println("\tCipher suite : " + sslSession.getCipherSuite());
                     System.out.println("Connection established with the backend");
                     // Start handling application content
                     OutputStream outputStream = sslSocket.getOutputStream();
@@ -70,39 +49,53 @@ public class SimpleNonBlockingClient {
                     printWriter.print(method + " /test/1 HTTP/1.1\r\n ");
                     printWriter.print("Accept: application/json\r\n");
                     printWriter.print("Connection: keep-alive\r\n");
-                    printWriter.print("Authorization: Bearer "+ Bearer +"\r\n");
+                    printWriter.print("Authorization: Bearer " + Bearer + "\r\n");
+                    int contentLength = payload.getBytes().length;
+                    if (contentLength == 0 | method == RequestMethods.GET) {
+                        System.out.println("Actual Content-Length: is: " + contentLength + " but " + method + " method so not sending Content-Length header");
+                        printWriter.print("\r\n");
+                        printWriter.flush();
+                        sslSocket.close();
+                        return;
+                    }
+                    System.out.println("Actual Content-Length is " + contentLength + " and sending Content-Length " + contentLength );
+                    printWriter.print("Content-Length: " + contentLength + "\r\n");
                     printWriter.print("Content-Type: application/json\r\n");
-                    printWriter.print("Content-Length: " + payload.getBytes().length + "\r\n");
                     printWriter.print("\r\n");
                     // Write payload data
                     printWriter.print(payload);
                     printWriter.print("\r\n");
                     printWriter.flush();
+                    Thread.sleep(1000);
                     // closing the socket after reading the response
                 } catch (Exception ex) {
                     ex.printStackTrace();
                 }
+            } catch (Exception ex) {
+                ex.printStackTrace();
             }
-
-        private static class ResponseReader implements Runnable {
-            private final SSLSocket sslSocket;
-            public ResponseReader(SSLSocket sslSocket) {
-                this.sslSocket = sslSocket;
-            }
-            @Override
-            public void run() {
-                try {
-                    System.out.println("Reading the response ...");
-                    InputStream inputStream = sslSocket.getInputStream();
-                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-                    String line;
-                    while((line = bufferedReader.readLine()) != null){
-                        System.out.println("Response : "+line);
-                    }
-                    inputStream.close();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    private class ResponseReader implements Runnable {
+        private final SSLSocket sslSocket;
+        public ResponseReader(SSLSocket sslSocket) {
+            this.sslSocket = sslSocket;
+        }
+        @Override
+        public void run() {
+            try {
+                System.out.println("Reading the response ...");
+                InputStream inputStream = sslSocket.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                while((line = bufferedReader.readLine()) != null){
+                    System.out.println("Response : "+line);
                 }
+                inputStream.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
         }
     }
